433a67b7a55fc95041fc52441b17d8664a762975
chebon254 <kelvinchebon90@gmail.com>
2022-10-26 17:34:03 +0300

    Code Changes

--- 14thOctoberV3/assets/main.js
+++ 14thOctoberV3/assets/main.js
@@ -1,46 +1,118 @@
-function nameCheck() {
-    let formName = document.forms["formRegister"]["formRegisterName"].value;
-    let nameMessage=document.getElementById('name-message');
-    if (formName == "") {
-        nameMessage.classList.add("active-message")
-        setTimeout(() => {
-            nameMessage.classList.remove("active-message");
-        }, 2000);
-    } else{
-        nameMessage.classList.remove("active-message");
-    }
-}
+// function nameCheck() {
+//     let formName = document.forms["formRegister"]["formRegisterName"].value;
+//     let nameMessage=document.getElementById('name-message');
+//     if (formName == "") {
+//         nameMessage.classList.add("active-message");
+//         setTimeout(() => {
+//             nameMessage.classList.remove("active-message");
+//         }, 2000);
+//     } else{
+//         nameMessage.classList.remove("active-message");
+//     }
+// }
 
-function emailCheck() {
-    let formEmail = document.forms["formRegister"]["formRegisterEmail"].value;
-    let emailMessage=document.getElementById('email-message');
-    if (formEmail == "") {
-        emailMessage.classList.add("active-message");
-        setTimeout(() => {
-            emailMessage.classList.remove("active-message");
-        }, 2000);
-    } else{
-        emailMessage.classList.remove("active-message");
-    }
-}
+// function emailCheck() {
+//     let formEmail = document.forms["formRegister"]["formRegisterEmail"].value;
+//     let emailMessage=document.getElementById('email-message');
+//     if (formEmail == "") {
+//         emailMessage.classList.add("active-message");
+//         setTimeout(() => {
+//             emailMessage.classList.remove("active-message");
+//         }, 2000);
+//     } else{
+//         emailMessage.classList.remove("active-message");
+//     }
+// }
 
 
+
+
+// let notifyMe=document.getElementById('button__submit');
+// let joinText=document.getElementById('add_success');
+// let SuccessText=document.getElementById('complete-success');
+
+// notifyMe.addEventListener('click',(e)=>{
+//     e.preventDefault();
+//     nameCheck();
+//     emailCheck();
+//     buttonCheck(); 
+// });
+
+
+// function buttonCheck() {
+//     let formName = document.forms["formRegister"]["formRegisterName"].value;
+//     let formEmail = document.forms["formRegister"]["formRegisterEmail"].value;
+//     if (formEmail == "" && formName == "") {
+//         return false;
+//     } else if (formEmail == "" || formName == "") {
+//         return false;
+//     } else{
+//         notifyMe.classList.add("success");
+//         joinText.style.display= "none";
+//         SuccessText.style.display= "block";
+//     }
+// }
 
 
 let notifyMe=document.getElementById('button__submit');
+const form = document.getElementById('formRegister');
+const username = document.getElementById('formRegisterName');
+const email = document.getElementById('formRegisterEmail');
 let joinText=document.getElementById('add_success');
 let SuccessText=document.getElementById('complete-success');
 
-notifyMe.addEventListener('click',(e)=>{
+notifyMe.addEventListener('click', (e) => {
     e.preventDefault();
-    nameCheck();
-    emailCheck();
-    buttonCheck(); 
+
+    validateInputs();
 });
 
+const setError = (element, message) => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error-message');
 
-function buttonCheck() {
-    notifyMe.classList.add("success");
-    joinText.style.display= "none";
-    SuccessText.style.display= "block";
+    errorDisplay.innerHTML = "<i class=\"fa-solid fa-triangle-exclamation\"></i><p>" + message + "</p>";
+    inputControl.classList.add('error');
+    inputControl.classList.remove('success');
 }
+
+const setSuccess = element => {
+    const inputControl = element.parentElement;
+    const errorDisplay = inputControl.querySelector('.error-message');
+
+    errorDisplay.innerHTML = "<i class=\"fa-solid fa-check\"></i>";
+    inputControl.classList.add('success');
+    inputControl.classList.remove('error');
+};
+
+const isValidEmail = email => {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+const validateInputs = () => {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+
+    console.log("Success");
+
+    if(usernameValue === '') {
+        setError(username, 'This field is required.');
+    } else {
+        setSuccess(username);
+    }
+
+    if(emailValue === '') {
+        setError(email, 'Please enter your email address.');
+    } else if (!isValidEmail(emailValue)) {
+        setError(email, 'Provide a valid email address');
+    } else {
+        setSuccess(email);
+    }
+
+    if (!usernameValue == '' && !emailValue == '' && !isValidEmail(emailValue) == false) {
+        notifyMe.classList.add("success");
+        joinText.style.display= "none";
+        SuccessText.style.display= "block";
+    }
+};
